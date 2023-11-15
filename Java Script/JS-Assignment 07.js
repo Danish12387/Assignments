137dfb51a3f90de1408a83a1a4a77af834392505
Danish12387 <danishshah2203651@gmail.com>
2023-11-16 00:04:02 +0500

    Assign updated

--- Java Script/JS-Assignment 07.js
+++ Java Script/JS-Assignment 07.js
@@ -3,6 +3,10 @@ import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasej
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-analytics.js";
 
+import {
+    getFirestore, collection, addDoc, getDocs
+} from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
+
 const firebaseConfig = {
     apiKey: "AIzaSyAHQyxWbE0KXf5RYphu1syMdxxLciEq-Jk",
     authDomain: "blog-app-ab7a6.firebaseapp.com",
@@ -17,13 +21,39 @@ const app = initializeApp(firebaseConfig);
 const database = getDatabase(app)
 const analytics = getAnalytics(app);
 const auth = getAuth(app)
+const db = getFirestore(app)
+
+const addDataToFS = collection(db, 'blogs')
 
 const form1 = document.getElementById('form-1')
-const userName = document.getElementById('userName')
 const form2 = document.getElementById('form-2')
 const signUpPage = document.getElementById('sign-up-page')
 const loginPage = document.getElementById('login-page')
 const dashboard = document.getElementById('dashboard')
+const userName = document.getElementById('userName')
+const dotsContainer = document.querySelector('.dots-container')
+const postBlogBtn = document.getElementById('post-blog-btn')
+const blogInput = document.getElementById('blog-input')
+const blogDesc = document.getElementById('textarea')
+const blog = document.getElementById('blog')
+
+setTimeout(() => {
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            const uid = user.uid;
+
+            signUpPage.style.display = 'none'
+            loginPage.style.display = 'none'
+            dotsContainer.style.display = 'none'
+            dashboard.style.display = 'block'
+
+            userName.innerHTML = user.email
+            getDataFS()
+        } else {
+            signUpPage.style.display = 'block'
+        }
+    });
+}, '1000');
 
 form1.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -44,11 +74,12 @@ form1.addEventListener('submit', (e) => {
     createUserWithEmailAndPassword(auth, userInfo.email, userInfo.password)
         .then((userCredential) => {
             const user = userCredential.user;
+
             set(ref(database, 'users/' + user.uid), {
                 username: userInfo.name,
                 email: userInfo.email
-            }
-            )
+            })
+
             alert('Signed Up successfully.')
             // window.location.href = 'https://serene-figolla-e2186d.netlify.app/'
             signUpPage.style.display = 'none'
@@ -62,6 +93,7 @@ form1.addEventListener('submit', (e) => {
         })
 })
 
+
 form2.addEventListener('submit', (e) => {
     e.preventDefault()
     const userInfo = {
@@ -74,12 +106,14 @@ form2.addEventListener('submit', (e) => {
             const user = userCredential.user;
 
             const date = new Date()
+            const dt = date.toLocaleString()
+
             update(ref(database, 'users/' + user.uid), {
-                last_login: date
+                last_login: dt
             })
-            alert('Loged In')
-            // window.location.href = 'https://serene-figolla-e2186d.netlify.app/'
+
             userName.innerHTML = userInfo.email
+
             signUpPage.style.display = 'none'
             loginPage.style.display = 'none'
             dashboard.style.display = 'block'
@@ -91,18 +125,9 @@ form2.addEventListener('submit', (e) => {
         });
 })
 
-const user = auth.currentUser;
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        const uid = user.uid;
-    } else {
-
-    }
-});
-
 const logout = document.getElementById('logout')
 
-logout.addEventListener('click',()=>{
+logout.addEventListener('click', () => {
     signOut(auth).then(() => {
 
         alert('Do you want to logout.')
@@ -111,16 +136,69 @@ logout.addEventListener('click',()=>{
         signUpPage.style.display = 'none'
         dashboard.style.display = 'none'
 
-      })
-      .catch((error) => {
+    })
+        .catch((error) => {
 
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        alert(errorMessage)
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage)
 
-      });
+        });
 })
 
+postBlogBtn.addEventListener('click', async () => {
+
+    if (!blogInput.value && !blogDesc.value) return alert('Enter your title and description!')
+
+    try {
+        const docRef = await addDoc(addDataToFS, {
+            Title: blogInput.value,
+            Desc: blogDesc.value
+        });
+
+        console.log("Document written with ID: ", docRef.id);
+
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
+
+    getDataFS()
+
+    blogDesc.value = ''
+    blogInput.value = ''
+})
+
+async function getDataFS() {
+    blog.innerHTML = null
+
+    const querySnapshot = await getDocs(addDataToFS);
+    querySnapshot.forEach((doc) => {    
+
+        const FSData = doc.data()
+
+        const blogDiv = `<div>
+        <h1>${FSData.Title}</h1>
+        <p>${FSData.Desc}</p>
+        <button id=${this.doc.id} class="del-btn">Delete</button>
+        </div>`
+        
+        blog.innerHTML += blogDiv
+        
+        const buttonElement = document.getElementById(this.doc.id);
+        
+        if (buttonElement) {
+            buttonElement.addEventListener('click', function() {
+
+                console.log(`Button with id ${this.doc.id} clicked!`);
+                
+            });
+        } else {
+            console.error(`Button with id ${this.doc.id} not found`);
+        }
+
+    });
+
+}
 
 const login = document.getElementById('login')
 const signup = document.getElementById('signup')
